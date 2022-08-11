a96ffbbe0fd4c33e6bbf4e912990a54fa4679b33
Roman Tarnakin <roman_tarnakin@epam.com>
2022-08-11 17:09:17 +0300

    add authorization token usage to client

--- src/index.tsx
+++ src/index.tsx
@@ -31,6 +31,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+localStorage.setItem('authorization_token', 'ajdhbW86VEVTVF9QQVNTV09SRA==');
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
